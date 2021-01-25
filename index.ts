4ab18f77e714aa44647f13fb01d89593b4b57a7d
Breno Novelli <bnojpa@gmail.com>
2021-01-25 11:50:25 -0300

    Custom props - Custom Regex and define which ones to format

--- index.ts
+++ index.ts
@@ -1,34 +1,128 @@
 interface HTMLProps {
   [key: string]: string | undefined;
 }
+interface FormatOptionsProps {
+  [key: string]: boolean;
+}
+interface FormatProps {
+  [key: string]: Function;
+}
 
-const HTMLtags: HTMLProps = {
+interface CustomRegex {
+  breakline?: RegExp;
+  htmlTags?: RegExp;
+  url?: RegExp;
+  phone?: RegExp;
+}
+interface ShouldFormatProps {
+  htmlTags?: boolean;
+  url?: boolean;
+  phone?: boolean;
+  breakline?: boolean;
+}
+interface FuncProps {
+  message: string;
+  customRegex?: CustomRegex;
+  shouldFormat?: ShouldFormatProps;
+}
+
+const regexDefault = {
+  breakline: /\n/g,
+  htmlTags: /([*~_]|```)([a-zA-Z0-9*_~].+?)\1(?!\w)/g,
+  url: /(?:(?:https|http):\/\/|\b(?:[a-z\d]+\.))[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/\S*)?/gim,
+  phone: /(?:(?:\(?(\d{2,11})\))|(?:(\d{2,11})))?[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. <\s]/gm,
+};
+
+const htmlTags: HTMLProps = {
   "*": "strong",
   "~": "strike",
   _: "em",
   "```": "code",
 };
 
-const formatterTextStyle = (match: string, p1: string, p2: string) =>
-  `<${HTMLtags[p1]}>${p2}</${HTMLtags[p1]}>`;
+const shouldFormatDefault = {
+  htmlTagsDefault: true,
+  urlDefault: true,
+  phoneDefault: true,
+  breaklineDefault: true,
+};
 
-const formatterLink = (match: string) =>
-  `<a href="${match}" target="_blank">${match}</a>`;
+const cleanURLPhone = (match: string) => match.replace(/[-. <]$/, "");
 
-const formatterPhone = (match: string) =>
-  `<a href="tel:${match}" target="_blank">${match.replace(/[-. <]$/, "")}</a>${
-    match.match(/[-. <]$/)?.[0]
-  }`;
+const formatter = {
+  /**
+   * Handle breaklines
+   */
+  breakline: () => "<br />",
+  /**
+   * Handle HTML Tags. Settings on array htmlTags
+   */
+  htmlTags: (match: string, p1: string, p2: string) =>
+    `<${htmlTags[p1]}>${p2}</${htmlTags[p1]}>`,
+  /**
+   * Handle links
+   */
+  link: (match: string) =>
+    `<a href="${match}" target="_blank"  rel="noreferrer noopener">${match}</a>`,
+  /**
+   * Handle phones numbers
+   * The contained Replaces are for moving unwanted characters out of the link
+   */
+  phone: (match: string) =>
+    `<a href="tel:${cleanURLPhone(
+      match
+    )}" target="_blank"  rel="noreferrer noopener">${cleanURLPhone(match)}</a>${
+      match.match(/[-. <]$/)?.[0]
+    }`,
+};
 
-const regexWhatsAppStyles = /([*~_]|```)([a-zA-Z0-9*_~].+?)\1(?!\w)/g;
-const regexURL = /(?:(?:https|http):\/\/|\b(?:[a-z\d]+\.))[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/\S*)?/gim;
-const regexPhone = /(?:(?:\(?(\d{2,11})\))|(?:(\d{2,11})))?[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. <\s]/gm;
+const formatChatMessage = ({
+  message,
+  customRegex,
+  shouldFormat,
+}: FuncProps) => {
+  const regex = {
+    breakline: customRegex?.breakline || regexDefault.breakline,
+    htmlTags: customRegex?.htmlTags || regexDefault.htmlTags,
+    url: customRegex?.url || regexDefault.url,
+    phone: customRegex?.phone || regexDefault.phone,
+  };
 
-const formatChatMessage = (messageBody: string) =>
-  messageBody
-    .replace(/\n/g, "<br />")
-    .replace(regexWhatsAppStyles, formatterTextStyle)
-    .replace(regexURL, formatterLink)
-    .replace(regexPhone, formatterPhone);
+  const formatOption: FormatOptionsProps = {
+    breakline:
+      typeof shouldFormat?.breakline !== "undefined"
+        ? shouldFormat.breakline
+        : shouldFormatDefault.breaklineDefault,
+    htmlTags:
+      typeof shouldFormat?.htmlTags !== "undefined"
+        ? shouldFormat.htmlTags
+        : shouldFormatDefault.htmlTagsDefault,
+    url:
+      typeof shouldFormat?.url !== "undefined"
+        ? shouldFormat.url
+        : shouldFormatDefault.urlDefault,
+
+    phone:
+      typeof shouldFormat?.phone !== "undefined"
+        ? shouldFormat.phone
+        : shouldFormatDefault.phoneDefault,
+  };
+
+  const format: FormatProps = {
+    breakline: (msg: string) =>
+      msg.replace(regex.breakline, formatter.breakline),
+    htmlTags: (msg: string) => msg.replace(regex.htmlTags, formatter.htmlTags),
+    url: (msg: string) => msg.replace(regex.url, formatter.link),
+    phone: (msg: string) => msg.replace(regex.phone, formatter.phone),
+  };
+
+  const formattedMessage = Object.keys(formatOption).reduce(
+    (acc, item) =>
+      formatOption[`${item}`] === true ? format[`${item}`](acc) : acc,
+    message
+  );
+
+  return formattedMessage;
+};
 
 export { formatChatMessage };
