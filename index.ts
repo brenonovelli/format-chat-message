c6b72a5760de96863955585a3cc46c4186937c7b
Breno Novelli <bnojpa@gmail.com>
2021-01-29 15:56:51 -0300

    Fix error on breaklinen and links

--- index.ts
+++ index.ts
@@ -27,10 +27,10 @@ interface FuncProps {
 }
 
 const regexDefault = {
-  breakline: /\n/g,
   htmlTags: /([*~_]|```)([a-zA-Z0-9*_~].+?)\1(?!\w)/g,
   url: /(?:(?:https|http):\/\/|\b(?:[a-z\d]+\.))[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/\S*)?/gim,
   phone: /(?:(?:\(?(\d{2,11})\))|(?:(\d{2,11})))?[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. <\s]/gm,
+  breakline: /\n/g,
 };
 
 const htmlTags: HTMLProps = {
@@ -51,10 +51,6 @@ const cleanURLPhone = (match: string) => match.replace(/[-. <]$/, "");
 
 const formatter = {
   /**
-   * Handle breaklines
-   */
-  breakline: () => "<br />",
-  /**
    * Handle HTML Tags. Settings on array htmlTags
    */
   htmlTags: (match: string, p1: string, p2: string) =>
@@ -74,6 +70,10 @@ const formatter = {
     )}" target="_blank"  rel="noreferrer noopener">${cleanURLPhone(match)}</a>${
       match.match(/[-. <]$/)?.[0]
     }`,
+  /**
+   * Handle breaklines
+   */
+  breakline: () => "<br />",
 };
 
 const formatChatMessage = ({
@@ -82,17 +82,13 @@ const formatChatMessage = ({
   shouldFormat,
 }: FuncProps) => {
   const regex = {
-    breakline: customRegex?.breakline || regexDefault.breakline,
     htmlTags: customRegex?.htmlTags || regexDefault.htmlTags,
     url: customRegex?.url || regexDefault.url,
     phone: customRegex?.phone || regexDefault.phone,
+    breakline: customRegex?.breakline || regexDefault.breakline,
   };
 
   const formatOption: FormatOptionsProps = {
-    breakline:
-      typeof shouldFormat?.breakline !== "undefined"
-        ? shouldFormat.breakline
-        : shouldFormatDefault.breaklineDefault,
     htmlTags:
       typeof shouldFormat?.htmlTags !== "undefined"
         ? shouldFormat.htmlTags
@@ -106,14 +102,18 @@ const formatChatMessage = ({
       typeof shouldFormat?.phone !== "undefined"
         ? shouldFormat.phone
         : shouldFormatDefault.phoneDefault,
+    breakline:
+      typeof shouldFormat?.breakline !== "undefined"
+        ? shouldFormat.breakline
+        : shouldFormatDefault.breaklineDefault,
   };
 
   const format: FormatProps = {
-    breakline: (msg: string) =>
-      msg.replace(regex.breakline, formatter.breakline),
     htmlTags: (msg: string) => msg.replace(regex.htmlTags, formatter.htmlTags),
     url: (msg: string) => msg.replace(regex.url, formatter.link),
     phone: (msg: string) => msg.replace(regex.phone, formatter.phone),
+    breakline: (msg: string) =>
+      msg.replace(regex.breakline, formatter.breakline),
   };
 
   const formattedMessage = Object.keys(formatOption).reduce(
